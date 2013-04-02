c2e2b5d28307e3f90e68224d3e49d09664ea3b71
shirlee <shirlee.berman@gmail.com>
2013-04-02 07:58:13 -0500

    setup testing env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+#  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, 'FACEBOOK_KEY', 'FACEBOOK_SECRET'
 
 end
